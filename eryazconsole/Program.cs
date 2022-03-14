71abedc3fb2fad9064528b211b510f4d670e1fc6
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2022-03-14 14:26:37 +0300

    eryaz yazılım test sorusu
    eryaz yazılım test sorusu

---
+++ eryazconsole/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+class Program
+{
+    /*
+    1'den 100'e kadar olan sayıları bir listeye/diziye ekleyen ve bu listeyi geri dönen bir
+    program/metot/fonksiyon hazırlanacak. Eğer sayı 4 veya 4'ün katı ise, sayı yerine “ER”; eğer
+    7 veya 7'nin katları ise sayı yerine “YAZ”; eğer hem 4'ün hem de 7'nin katı ise sayı yerine
+    “ERYAZ” eklenmelidir.
+    */
+    private static int multiplesOffour = 4;
+    private static int multiplesOfSeven = 7;
+    private static int zero = 0;
+
+ 
+    static void Main()
+    { 
+            var numberList = Enumerable.Range(1, 100).ToArray();
+
+            foreach (var item in numberList)
+            {
+                if(CheckContains( item))
+                {
+                 ErYaz();
+                }
+                
+                else if (CheckMultiplesOfFour(item))
+                {
+                    Er();
+                }
+                else if (CheckMultiplesOfSeven(item))
+                {
+                    Yaz();
+                }
+                else
+                {
+                  Console.WriteLine(item.ToString());
+                } 
+             } 
+    }
+
+     static bool CheckContains(int item)
+    {
+        return (item % multiplesOffour == zero & item % multiplesOfSeven == zero);
+
+    }
+
+     static bool CheckMultiplesOfFour(int item)
+    {
+        return (item % multiplesOffour == zero);
+
+    }
+    private static bool CheckMultiplesOfSeven(int item)
+    {
+        return (item % multiplesOfSeven == zero);
+
+    }
+    static void Er()
+    {
+        Console.WriteLine("ER");
+    }
+    static void Yaz()
+    {
+        Console.WriteLine("YAZ");
+    }
+    static void ErYaz()
+    {
+        Console.WriteLine("ERYAZ");
+    }
+}
